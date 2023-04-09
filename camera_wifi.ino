a92ca8d959b26049c14dbc8a9bf7854b2bb84783
İkranur Güler <62421679+gulerikra@users.noreply.github.com>
2023-04-09 12:01:56 +0300

    Update camera_wifi.ino

--- camera_wifi.ino
+++ camera_wifi.ino
@@ -1,92 +1,54 @@
-#include <WebServer.h>
-#include <WiFi.h>
-#include <esp32cam.h>
+#include <WebServer.h>    // Web sunucusu kütüphanesi
+#include <WiFi.h>         // WiFi kütüphanesi
+#include <esp32cam.h>     // ESP32-CAM kütüphanesi
  
-const char* WIFI_SSID = "ikra";
-const char* WIFI_PASS = "iKranur8";
+const char* WIFI_SSID = "ikra";    // WiFi ağ adı
+const char* WIFI_PASS = "iKranur8";    // WiFi şifresi
  
-WebServer server(80);
+WebServer server(80);    // Web sunucusu nesnesi oluşturma
  
- 
+// Farklı çözünürlük seçeneklerinin tanımlanması
 static auto loRes = esp32cam::Resolution::find(320, 240);
 static auto midRes = esp32cam::Resolution::find(350, 530);
 static auto hiRes = esp32cam::Resolution::find(800, 600);
+ 
+// JPEG görüntülerin sunulması için bir işlev
 void serveJpg()
 {
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
+  auto frame = esp32cam::capture();    // Görüntü yakalama işlemi
+  if (frame == nullptr) {    // Görüntü yakalama hatası kontrolü
     Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
+    server.send(503, "", "");    // Sunucu hatası yanıtı
     return;
   }
   Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
+                static_cast<int>(frame->size()));    // Görüntü bilgilerinin seri yazdırılması
  
-  server.setContentLength(frame->size());
-  server.send(200, "image/jpeg");
-  WiFiClient client = server.client();
-  frame->writeTo(client);
+  server.setContentLength(frame->size());    // Sunulan verinin boyutunun ayarlanması
+  server.send(200, "image/jpeg");    // Başarılı yanıt kodu ve MIME türü ayarlanması
+  WiFiClient client = server.client();    // Sunucuya bağlı olan istemcinin alınması
+  frame->writeTo(client);    // Görüntünün istemciye gönderilmesi
 }
  
+// Düşük çözünürlük için işlev
 void handleJpgLo()
 {
-  if (!esp32cam::Camera.changeResolution(loRes)) {
+  if (!esp32cam::Camera.changeResolution(loRes)) {    // Çözünürlük değiştirme hatası kontrolü
     Serial.println("SET-LO-RES FAIL");
   }
-  serveJpg();
+  serveJpg();    // JPEG görüntü işleme işlevinin çağrılması
 }
  
+// Yüksek çözünürlük için işlev
 void handleJpgHi()
 {
-  if (!esp32cam::Camera.changeResolution(hiRes)) {
+  if (!esp32cam::Camera.changeResolution(hiRes)) {    // Çözünürlük değiştirme hatası kontrolü
     Serial.println("SET-HI-RES FAIL");
   }
-  serveJpg();
+  serveJpg();    // JPEG görüntü işleme işlevinin çağrılması
 }
  
+// Orta çözünürlük için işlev
 void handleJpgMid()
 {
-  if (!esp32cam::Camera.changeResolution(midRes)) {
-    Serial.println("SET-MID-RES FAIL");
-  }
-  serveJpg();
-}
- 
- 
-void  setup(){
-  Serial.begin(115200);
-  Serial.println();
-  {
-    using namespace esp32cam;
-    Config cfg;
-    cfg.setPins(pins::AiThinker);
-    cfg.setResolution(hiRes);
-    cfg.setBufferCount(2);
-    cfg.setJpeg(80);
- 
-    bool ok = Camera.begin(cfg);
-    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
-  }
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-  Serial.print("http://");
-  Serial.println(WiFi.localIP());
-  Serial.println("  /cam-lo.jpg");
-  Serial.println("  /cam-hi.jpg");
-  Serial.println("  /cam-mid.jpg");
- 
-  server.on("/cam-lo.jpg", handleJpgLo);
-  server.on("/cam-hi.jpg", handleJpgHi);
-  server.on("/cam-mid.jpg", handleJpgMid);
- 
-  server.begin();
-}
- 
-void loop()
-{
-  server.handleClient();
-}
+  if (!esp32cam::Camera.changeResolution(midRes)) {    // Çözünürlük değiştirme hatası kontrolü
