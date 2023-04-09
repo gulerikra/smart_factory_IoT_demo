668dcd54cbb2a0d15d06120fe5deda665c736e30
İkranur Güler <62421679+gulerikra@users.noreply.github.com>
2023-04-09 13:43:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,98 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const { exec } = require('child_process');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Set the initial image filename
+// İlk resim dosya adını ayarla
 let imageFilename = "image.jpg";
 
 exec('python camera.py', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
 
-// Periodically check for new images and update the filename
+// Periyodik olarak yeni görüntüleri kontrol edin ve dosya adını güncelleyin
 setInterval(() => {
   let newFilename = null;
-  // Your code to check for new image filenames goes here
-  // For example, you could check the modification time of the files in the "public" directory
-  // If a new image is found, set newFilename to the name of the new file
-
+  // Yeni resim dosya adlarını kontrol etme kodunuz buraya gelir
+  // Örneğin, "public" dizinindeki dosyaların değiştirilme zamanını kontrol edebilirsiniz.
+  // Yeni bir görüntü bulunursa, yeni dosyanın adını yeni Dosyaadı olarak ayarlayın
   if (newFilename !== null && newFilename !== imageFilename) {
-    // A new image has been found, update the filename and notify the console
+    // Yeni bir resim bulundu, dosya adını güncelleyin ve konsola bildirin
     console.log(`New image found: ${newFilename}`);
     imageFilename = newFilename;
 
-    // Send a message to all connected clients to update the image
+    // Görüntüyü güncellemek için tüm bağlı istemcilere bir mesaj gönderin
     io.emit('image', imageFilename);
   }
 }, 1000);
 
-// Serve the webpage and dynamically update the displayed image
+// Web sayfasını sunun ve görüntülenen resmi dinamik olarak güncelleyin
 const server = app.listen(3000, function () {
-  console.log('Sunucu 3000 numaralı portta çalışıyor.')
-})
+  console.log('Sunucu 3000 numaralı portta çalışıyor.');
+});
 
-// Set up Socket.IO
+// Socket.IO'yu kurun
 const io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-  // Send the current image filename to the client when they connect
+// Bağlandıklarında istemciye geçerli resim dosya adını gönder
   socket.emit('image', imageFilename);
 });
 
-// Send the HTML file to the client
+// HTML dosyasını istemciye gönder
 app.get('/', function (req, res) {
   res.send(`
     <!DOCTYPE html>
     <html>
-    <head>
-      <title>Web Kamera</title>
-      <meta http-equiv="refresh" content="3"> <!-- Refresh the page every 3 seconds -->
-      <script src="/socket.io/socket.io.js"></script>
-      <script>
-        // Connect to the Socket.IO server
-        const socket = io();
+      <head>
+        <title>Web Kamera</title>
+        <meta http-equiv="refresh" content="3"> <!-- Refresh the page every 3 seconds -->
+        <script src="/socket.io/socket.io.js"></script>
+        <script>
+          // Connect to the Socket.IO server
+          const socket = io();
 
-        // When the image message is received, update the image on the page
-        socket.on('image', function (filename) {
-          const img = document.querySelector('img');
-          img.src = '/' + filename;
-        });
-      </script>
-    </head>
-    <body>
-      <h1>Web Kamera</h1>
-      <img src="/${imageFilename}" alt="Çekilen Resim" width="640" height="480">
-    </body>
+          // When the image message is received, update the image on the page
+          socket.on('image', function (filename) {
+            const img = document.querySelector('img');
+            img.src = '/' + filename;
+          });
+        </script>
+      </head>
+      <body>
+        <h1>Web Kamera</h1>
+        <img src="/${imageFilename}" alt="Çekilen Resim" width="640" height="480">
+      </body>
     </html>
-  `)
+  `);
+});
+
+// http modülünü kullanarak yeni bir sunucu oluşturun
+const http = require('http');
+const port = 4000;
+
+const requestHandler = (request, response) => {
+  let body = '';
+  request.on('data', (data) => {
+    body += data;
+  });
+  request.on('end', () => {
+    console.log(body);
+  });
+  response.end('OK');
+};
+
+const newServer = http.createServer(requestHandler);
+
+newServer.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err);
+  }
+  console.log(`New server is listening on ${port}`);
 });
